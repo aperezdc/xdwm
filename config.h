64b88df8d9e428426152ba8e10267d53a178997c
Adrian Perez de Castro <aperez@igalia.com>
2012-11-08 15:48:16 +0200

    Revert font to an X bitmap font
    As nice as it is to have Xft support, passing down the font names to dmenu
    and other tools might not work because those tools may not support Xft in
    their side. The safe approach is to fall-back to normal X fonts for the
    time being.

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-terminus-medium-r-*-*-16-*-*-*-*-*-*-*";
+static const char font[]            = "-*-gohufont-medium-r-normal-*-14-*-*-*-*-*-iso10646-1";
 static const char normbordercolor[] = "#444444";
 static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
