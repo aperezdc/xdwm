740060a479201d97ba080b44f79f73288ad6b3e1
Adrian Perez de Castro <aperez@igalia.com>
2012-12-13 18:46:06 +0100

    Do not make The Gimp windows float by default
    Newer versions of The Gimp include a single-window mode which is
    particularly suitable for use in tiling window managers -- specially
    in fullscreen/monocle mode. Thus, its windows do not need to be treated as
    floating by default.

--- config.h
+++ config.h
@@ -22,7 +22,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Gcalctool",NULL,       NULL,       0,            True,        -1 },
 };
 
